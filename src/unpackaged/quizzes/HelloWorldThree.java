327f1aa08ad863c8d7004cac4d0e52516c69b907
Arwa Sayegh <ts11ass@leeds.ac.uk>
2018-01-06 20:06:46 +0000

    Check

---
+++ src/unpackaged/quizzes/HelloWorldThree.java
@@ -0,0 +1,7 @@
+public class HelloWorldThree {
+	public static void main (String args[]) {
+		Point p1 = new Point();
+		double newxy = p1.x * p1.y;
+		System.out.println("Second java: Hello World and new point is " + (newxy + 1));
+	}
+}
